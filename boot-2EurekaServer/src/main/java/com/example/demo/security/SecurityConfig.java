7148e6b15345dc9f45b870678f113bee7dd0ce08
kimyunseok <neobnsrnd5@gmail.com>
2024-11-18 17:46:11 +0900

    eureka server

---
+++ boot-2EurekaServer/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.demo.security;
+
+import java.util.Collection;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		
+		http.csrf((auth) -> auth.disable()); // csrf off
+		http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated()); //all request need authentication
+		http.httpBasic(Customizer.withDefaults());
+		
+		return http.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		UserDetails user1 = User.builder()
+				.username("admin")
+				.password(bCryptPasswordEncoder().encode("1234"))
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user1);
+	}
+}

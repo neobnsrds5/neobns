7c03d4af7942eeed9b99ec4c1c7d8513145c5216
ThebeginnerJiyeon <neobns.rnd.2@gmail.com>
2024-11-21 07:54:15 +0900

    feat index문서 추가

--- Boot-4-AdminServer/src/main/java/com/neo/adminserver/configuration/WebConfig.java
+++ Boot-4-AdminServer/src/main/java/com/neo/adminserver/configuration/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration	
 public class WebConfig implements WebMvcConfigurer{
-
+	// TODO: 상세 설정 추가할 것
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOriginPatterns("*")

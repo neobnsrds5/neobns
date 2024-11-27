8145d946f5a334321ba8bc85787e8d2622fc5abe
kimyunseok <neobnsrnd5@gmail.com>
2024-11-27 12:07:01 +0900

    mapper

--- quitstart/src/main/java/com/example/neobns/mapper/QuickMapper.java
+++ quitstart/src/main/java/com/example/neobns/mapper/QuickMapper.java
@@ -1,5 +1,6 @@
 package com.example.neobns.mapper;
 
+import com.example.neobns.dto.AccountDTO;
 import com.example.neobns.dto.ItemDto;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -10,4 +11,5 @@ public interface QuickMapper {
     ItemDto findById(String id);
     List<ItemDto> findAll();
     void addItem(ItemDto item);
+    void addAccount(AccountDTO dto);
 }

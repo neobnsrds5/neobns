565091e6a84e0b9b8de393d68af4b24b59ee53b2
ThebeginnerJiyeon <neobns.rnd.2@gmail.com>
2024-12-01 04:04:21 +0900

    feat: Redisson 캐시 설정 및 간단한 메서드 처리

--- quitstart/src/main/java/com/example/neobns/service/QuickService.java
+++ quitstart/src/main/java/com/example/neobns/service/QuickService.java
@@ -5,6 +5,8 @@ import com.example.neobns.dto.ItemDto;
 import com.example.neobns.mapper.QuickMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,14 +17,16 @@ import java.util.List;
 public class QuickService {
 
     private final QuickMapper mapper;
-
+    
     public boolean registerItem(ItemDto itemDto){
         log.info("Service....");
         mapper.addItem(itemDto);
         return true;
     }
 
+    @Cacheable(value = "item:id", key="#id")
     public ItemDto getItemById(String id){
+    	System.out.println("getItemById : " + id + " 실행됨~");
         ItemDto itemDto = mapper.findById(id);
         return itemDto;
     }

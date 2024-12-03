d1b219a7cd977aab5296a6c1684b40585a4a28d8
neobnsrds5 <neobnsrnd5@gmail.com>
2024-12-03 15:04:34 +0900

    props

--- transfer/src/main/java/com/example/neobns/config/ConsumerConfig.java
+++ transfer/src/main/java/com/example/neobns/config/ConsumerConfig.java
@@ -4,21 +4,26 @@ import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class ConsumerConfig {
+	
+	private RabbitProp prop;
 
     @Bean
     public Queue queue(){
-        return new Queue("example-queue", true);
+        return new Queue(prop.getQueue(), true);
     }
 
     @Bean
     public DirectExchange exchange(){
-        return new DirectExchange("example-exchange");
+        return new DirectExchange(prop.getExchange());
     }
 
     @Bean
     public Binding binding(Queue queue, DirectExchange exchange){
-        return BindingBuilder.bind(queue).to(exchange).with("example-routing-key");
+        return BindingBuilder.bind(queue).to(exchange).with(prop.getRoutingKey());
     }
 }

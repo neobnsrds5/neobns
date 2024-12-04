28f2f6281dea72795802133adab15b48b7f2b9fc
neobnsrds2 <neobns.rnd.2@gmail.com>
2024-12-04 18:13:03 +0900

    fix plantUML 수정 중

--- Boot-4-AdminServer/src/main/java/com/neo/adminserver/service/LogService.java
+++ Boot-4-AdminServer/src/main/java/com/neo/adminserver/service/LogService.java
@@ -1,6 +1,10 @@
 package com.neo.adminserver.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class LogService {
 	
 	private final LogMapper logMapper;
+	private Map<String, String> imgUrlMap = new HashMap<>();
 	
 	public List<LogDTO> findSlowByPage(){
 		return logMapper.findSlowByPage();
@@ -25,5 +30,42 @@ public class LogService {
 	
 	public List<LogDTO> findByTraceId(String traceId){
 		return logMapper.findByTraceId(traceId);
+	}
+
+	public String buildPlantUML(String traceID, List<LogDTO> logList) {
+		
+		if (imgUrlMap.containsKey(traceID)) {
+			return imgUrlMap.get(traceID);
+		}
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append("@startuml").append(System.lineSeparator());
+		
+		for (int i = 0; i <= logList.size()-1; i++) {
+			String addedString;
+			
+			if (i<=logList.size()-2) {
+				addedString = logList.get(i).getCallerClass()+ " -> " + logList.get(i+1).getCallerClass() + " : " + logList.get(i).getCallerMethod();
+			} else {
+				addedString = logList.get(i).getCallerClass()+ " -> " + logList.get(0).getCallerClass() + " : " + logList.get(i).getCallerMethod();
+			}
+			
+			
+			
+			builder.append(addedString).append(System.lineSeparator());
+			
+		}
+		
+		
+		builder.append("@enduml");
+		String url = builder.toString();
+		
+		imgUrlMap.put(traceID, url);
+		
+		System.out.println("made string : " + url);
+		
+		
+		
+		return null;
 	}
 }

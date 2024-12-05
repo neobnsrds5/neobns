87917264b006eb2bde3912f89b453651497aec9d
neobnsrnd4 <neobns.rnd.4@gmail.com>
2024-12-05 10:42:27 +0900

    adminserver resource 파일 gateway 통해서 라우팅

--- Boot-4-AdminServer/src/main/java/com/neo/adminserver/configuration/WebConfig.java
+++ Boot-4-AdminServer/src/main/java/com/neo/adminserver/configuration/WebConfig.java
@@ -2,34 +2,25 @@ package com.neo.adminserver.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	// TODO: 상세 설정 추가할 것
-//	@Override
-//	public void addCorsMappings(CorsRegistry registry) {
-//		registry.addMapping("/**")
-//		.allowedOrigins("http://")
-//		.allowedHeaders("*")
-//		.allowedMethods("*")
-//		.allowCredentials(true);
-//	}
 
-	public WebMvcConfigurer corsconConfigurer() {
-		return new WebMvcConfigurer() {
+	// /static/assets를 읽어오지 못하길래 아래와 같이 명시해줌
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/static/assets/");
+    }
 
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:4200","*")
-						.allowedHeaders("*")
-						.allowedMethods("*")
-						.allowCredentials(true);
-			}
-
-		};
-
-	}
-
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:4200", "*")
+//                .allowedHeaders("*")
+//                .allowedMethods("*")
+//                .allowCredentials(true);
+//    }
 }

d2f142452f0c638ec4552a06edf9ac910e271251
ThebeginnerJiyeon <neobns.rnd.2@gmail.com>
2024-12-09 00:51:17 +0900

    fix jobparamter 유일한 값이 되도록 변경

--- quitstart/src/main/java/com/example/neobns/controller/BatchController.java
+++ quitstart/src/main/java/com/example/neobns/controller/BatchController.java
@@ -16,32 +16,35 @@ import lombok.RequiredArgsConstructor;
 public class BatchController {
 	private final JobLauncher jobLauncher;
 	private final JobRegistry jobRegistry;
-	
+
 	@GetMapping("/batch/dbtoapi/{value}")
-	public String firstApi(@PathVariable("value") String value) throws Exception{
-		JobParameters jobParameters = new JobParametersBuilder()
-				.addString("dbtoapi", value)
-				.toJobParameters();
-		
+	public String firstApi(@PathVariable("value") String value) throws Exception {
+
+		String uniqVal = value + System.currentTimeMillis();
+
+		JobParameters jobParameters = new JobParametersBuilder().addString("dbtoapi", uniqVal).toJobParameters();
+
 		jobLauncher.run(jobRegistry.getJob("dbToApiJob"), jobParameters);
 		return "OK";
 	}
-	
+
 	@GetMapping("/batch/dbtodb/{value}")
 	public String dbtodbTest(@PathVariable("value") String value) throws Exception {
-		JobParameters jobParameters = new JobParametersBuilder()
-				.addString("dbtodb", value)
-				.toJobParameters();
+
+		String uniqVal = value + System.currentTimeMillis();
+
+		JobParameters jobParameters = new JobParametersBuilder().addString("dbtodb", uniqVal).toJobParameters();
 		jobLauncher.run(jobRegistry.getJob("dbCopyJob"), jobParameters);
 		return "OK";
 	}
-	
+
 	@GetMapping("/batch/filetodb/{value}")
 	public String fileToDbTest(@PathVariable("value") String value) {
-		JobParameters jobParameters = new JobParametersBuilder()
-				.addString("filetodb", value)
-				.toJobParameters();
-		
+
+		String uniqVal = value + System.currentTimeMillis();
+
+		JobParameters jobParameters = new JobParametersBuilder().addString("filetodb", uniqVal).toJobParameters();
+
 		try {
 			jobLauncher.run(jobRegistry.getJob("fileToDBJob"), jobParameters);
 			return "OK";
@@ -49,13 +52,14 @@ public class BatchController {
 			return "FAIL";
 		}
 	}
-	
+
 	@GetMapping("/batch/logtodb/{value}")
 	public String logToDbTest(@PathVariable("value") String value) {
-		JobParameters jobParameters = new JobParametersBuilder()
-				.addString("logtodb", value)
-				.toJobParameters();
-		
+
+		String uniqVal = value + System.currentTimeMillis();
+
+		JobParameters jobParameters = new JobParametersBuilder().addString("logtodb", uniqVal).toJobParameters();
+
 		try {
 			jobLauncher.run(jobRegistry.getJob("logToDBJob"), jobParameters);
 			return "OK";
@@ -63,5 +67,5 @@ public class BatchController {
 			return "FAIL";
 		}
 	}
-	
+
 }

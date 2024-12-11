3d639046a589be30a0ed7dc0fca9121ee674978f
neobnsrds2 <neobns.rnd.2@gmail.com>
2024-12-11 12:31:05 +0900

    fix: redis 원격레디스 서버로 변경
    캐시 주석처리되어있어 다시 해제

--- quitstart/src/main/java/com/example/neobns/service/QuickService.java
+++ quitstart/src/main/java/com/example/neobns/service/QuickService.java
@@ -29,44 +29,45 @@ public class QuickService {
 	private final TransferRepository transferRepository;
 	private final CacheManager cacheManager;
 
-//	@CachePut(value = "item:id", key = "#itemDto.id")
+	@CachePut(value = "item:id", key = "#itemDto.id")
 	public boolean registerItem(ItemDto itemDto) {
 		mapper.addItem(itemDto);
 		return true;
 	}
 
-//	@Cacheable(value = "item:id", key = "#id")
+	@Cacheable(value = "item:id", key = "#id")
 	public ItemDto getItemById(String id) {
 		ItemDto itemDto = mapper.findById(id);
 		return itemDto;
 	}
 	
-//	@Cacheable(value = "item:id")
+	@Cacheable(value = "item:id")
 	public List<ItemDto> getAll() {
 		List<ItemDto> results = mapper.findAll();
 		return results;
 	}
 
-//	@CachePut(value = "account:id", key = "#dto.id")
+	@CachePut(value = "account:id", key = "#dto.id")
 	public void addAccount(AccountDTO dto) {
 		// TODO Auto-generated method stub
 		mapper.addAccount(dto);
 	}
 	
-//	@Schedule
+	@Scheduled(fixedRate = 100_000)
+	@CacheEvict(value = "account:id", allEntries = true)
 	public void clearAllAccountCache() {
 		System.out.println("All account cash evicted. fixedRate = 10000");
 		
 	}
 	
-//	@CachePut(value = "account:id", key = "#account.id")
+	@CachePut(value = "account:id", key = "#account.id")
 	public Account addUpdateAccountJPA(Account account) {
 		Account returnAccount = accountRepository.save(account);
 		return returnAccount;
 		
 	}
 
-//	@Cacheable(value = "account:id", key = "#id")
+	@Cacheable(value = "account:id", key = "#id")
 	public Account getAccountByIdJPA(long id) {
 		return accountRepository.findById(id).get();
 	}

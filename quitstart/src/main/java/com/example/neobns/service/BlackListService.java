bf31a60afd9cafd23db7ac4484bcbe9e15c8ecd7
ThebeginnerJiyeon <neobns.rnd.2@gmail.com>
2024-12-13 01:16:59 +0900

    feat: blacklist ip 제외 기능 추가

---
+++ quitstart/src/main/java/com/example/neobns/service/BlackListService.java
@@ -0,0 +1,46 @@
+package com.example.neobns.service;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import kotlin.ranges.IntRange;
+
+@Service
+public class BlackListService {
+
+	private final JdbcTemplate spiderTemplate;
+
+	public BlackListService(JdbcTemplate spiderTemplate) {
+		super();
+		this.spiderTemplate = spiderTemplate;
+	}
+
+	public boolean isBlackListed(String ip) {
+
+		ip = ip.trim();
+		String sql = "SELECT COUNT(*) FROM FWK_BLACKLIST WHERE BLACK_LIST_IP = ?";
+		Integer count = spiderTemplate.queryForObject(sql, Integer.class, ip);
+
+		return (count != null && count > 0);
+
+	}
+
+	public String getRealIp(HttpServletRequest request) {
+
+		String clientIp = request.getHeader("X-Forwarded-For");
+		if (clientIp != null && !clientIp.isEmpty() && "unknown".equalsIgnoreCase(clientIp)) {
+			return clientIp.split(",")[0].trim();
+		}
+
+		clientIp = request.getHeader("X-Real-IP");
+		if (clientIp != null && !clientIp.isEmpty() && "unknown".equalsIgnoreCase(clientIp)) {
+			return clientIp.trim();
+		}
+
+		return request.getRemoteAddr();
+	}
+
+}

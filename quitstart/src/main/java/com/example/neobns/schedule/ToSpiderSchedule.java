2e8c81268392710f1b4e7076f742502054e1d86c
neobnsrnd1 <neobns.rnd1@gmail.com>
2024-12-16 14:04:35 +0900

    Add files via upload

---
+++ quitstart/src/main/java/com/example/neobns/schedule/ToSpiderSchedule.java
@@ -0,0 +1,37 @@
+package com.example.neobns.schedule;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class ToSpiderSchedule {
+	private final JobLauncher jobLauncher;
+	private final JobRegistry jobRegistry;
+
+//	@Scheduled(cron = "*/30 * * * * * ", zone = "Asia/Seoul")
+	public void runSpiderSchedule() throws Exception {
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss");
+		String date = LocalDateTime.now().format(formatter);
+
+		JobParameters jobParameters = new JobParametersBuilder().addString("date", date)
+				.addLong("timestamp", System.currentTimeMillis())
+				.toJobParameters();
+
+		jobLauncher.run(jobRegistry.getJob("dbToSpiderErrorJob"), jobParameters);
+
+	}
+
+}

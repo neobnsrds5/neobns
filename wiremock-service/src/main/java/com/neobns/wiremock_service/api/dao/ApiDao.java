e6ad514fd1ed6b4f499653d257484cceec6bb49f
neobnsrds5 <neobnsrnd5@gmail.com>
2024-12-18 11:53:31 +0900

    wiremock

---
+++ wiremock-service/src/main/java/com/neobns/wiremock_service/api/dao/ApiDao.java
@@ -0,0 +1,19 @@
+package com.neobns.wiremock_service.api.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.neobns.wiremock_service.api.vo.ApiVO;
+
+@Mapper
+public interface ApiDao {
+	List<ApiVO> findAll();
+	ApiVO findById(int id);
+	List<ApiVO> findByIds(List<Integer> ids);
+	void saveApi(ApiVO apiVO);
+	void updateCheckedStatus(ApiVO apiVO);
+	void changeResponseStatusById(ApiVO apiVO);
+	void toggleResponseStatusById(int id);
+	void toggleResponseStatusByIds(List<Integer> ids);
+}

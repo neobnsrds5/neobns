bb3a77f287b6dfef0440139e821ce2531f67591f
neobnsrnd4 <neobns.rnd.4@gmail.com>
2024-12-19 10:38:06 +0900

    batch 수정

--- quitstart/src/main/java/com/example/neobns/QuitstartApplication.java
+++ quitstart/src/main/java/com/example/neobns/QuitstartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableScheduling
 @EnableCaching
 public class QuitstartApplication {

63fdf818e204215faa48537f4748c11c66e74e9a
neobnsrnd4 <neobns.rnd.4@gmail.com>
2025-01-09 16:00:23 +0900

    Update BatchController.java

--- Boot-04-AdminServer/src/main/java/com/neo/adminserver/controller/BatchController.java
+++ Boot-04-AdminServer/src/main/java/com/neo/adminserver/controller/BatchController.java
@@ -25,80 +25,76 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/batch")
 public class BatchController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(BatchController.class);
-	
+
 	private final BatchService batchService;
-	
+
 	@GetMapping("/jobList")
 	public String list(Model model, @RequestParam HashMap<String, Object> paramMap, PageUtil pgtl) {
-		
+
 		String todayDate = DateUtil.getDate("yyyy-MM-dd");
-		
-	    logger.info("----------------------------------------------------");
-	    logger.info("/batch/jobList");
-	    logger.info("paramMap : " + paramMap);   
-	    logger.info("----------------------------------------------------");
-	        
-	    //------------------------------------------------------------------------
-	    // SearchMap Init
-	    //------------------------------------------------------------------------
-	    SearchMap searchMap = new SearchMap(paramMap);
-	    searchMap.initParam("status", "COMPLETED");
-	    searchMap.initParam("jobName", "");
-	    searchMap.initParam("startDate", todayDate);
-	    searchMap.initParam("endDate", todayDate);
-	    
-	    //------------------------------------------------------------------------
-	    // pageLink init
-	    //------------------------------------------------------------------------
-	    int total = batchService.listCount(searchMap);
-	    pgtl.init(total, "/batch/jobList", searchMap.getParams());
-	    searchMap.setPgtl(pgtl);
-	    
+
+		logger.info("----------------------------------------------------");
+		logger.info("/batch/jobList");
+		logger.info("paramMap : " + paramMap);
+		logger.info("----------------------------------------------------");
+
+		// ------------------------------------------------------------------------
+		// SearchMap Init
+		// ------------------------------------------------------------------------
+		SearchMap searchMap = new SearchMap(paramMap);
+		searchMap.initParam("status", "COMPLETED");
+		searchMap.initParam("jobName", "");
+		searchMap.initParam("startDate", todayDate);
+		searchMap.initParam("endDate", todayDate);
+
+		// ------------------------------------------------------------------------
+		// pageLink init
+		// ------------------------------------------------------------------------
+		int total = batchService.listCount(searchMap);
+		pgtl.init(total, "/batch/jobList", searchMap.getParams());
+		searchMap.setPgtl(pgtl);
+
 		List<BatchJobInstanceDTO> list = batchService.list(searchMap);
 
-		String[] status = {"COMPLETED", "STARTING", "STARTED", "STOPPING", "STOPPED", "FAILED", "UNKNOWN"};
-		
-		for(int i=0; i<total; i++) {
-		    logger.info("JobInstance : {}", list.get(i).toString());
-		}
-		
+		String[] status = { "COMPLETED", "STARTING", "STARTED", "STOPPING", "STOPPED", "FAILED", "UNKNOWN" };
+
 		model.addAttribute("list", list);
 		model.addAttribute("searchMap", searchMap);
 		model.addAttribute("statusList", status);
-		return "batch/jobList";		
+		return "batch/jobList";
 	}
-	
+
 	@GetMapping("/jobDetail")
 	public String jobDetail(Model model, BatchJobInstanceDTO paramVo) {
-		
+
 		BatchJobInstanceDTO job = null;
 		List<BatchStepExecutionDTO> steps = null;
-		
+
 		job = batchService.selectJobDetail(paramVo.getInstanceId());
-		
+
 		logger.info("BatchJobInstanceEntity : " + job.toString());
-		
-		if(job.getExec() != null) {
+
+		if (job.getExec() != null) {
 			steps = batchService.listStepDetail(job.getExec().getExecutionId());
 		}
-		
+
 		model.addAttribute("job", job);
 		model.addAttribute("steps", steps);
-		
-		return "batch/jobDetail";		
-	}	
-	
+
+		return "batch/jobDetail";
+	}
+
 	@GetMapping("/stepDetail")
 	public String stepDetail(Model model, BatchJobExecutionDTO paramVo) {
-		
+
 		BatchJobExecutionDTO job = batchService.selectStepDetail(paramVo.getExecutionId());
-		
+
 		logger.info("BatchJobExecutionEntity : " + job.toString());
-		
+
 		model.addAttribute("job", job);
-		return "batch/stepDetail";		
-	}	
+		return "batch/stepDetail";
+	}
 
 }

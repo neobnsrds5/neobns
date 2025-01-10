f71c1d2c2ce8d72513959068d8f4768e671802b5
neobnsrds5 <neobnsrnd5@gmail.com>
2025-01-10 13:53:31 +0900

    parameterSave.js modified

--- Boot-04-AdminServer/src/main/resources/static/js/parameterSave.js
+++ Boot-04-AdminServer/src/main/resources/static/js/parameterSave.js
@@ -19,31 +19,34 @@ function initSearchState(){
     sessionStorage.setItem("searchState", JSON.stringify(value));
 }
 
-const collectParams = () => {
-
-};
-
 document.addEventListener('DOMContentLoaded', function(){
     const searchState = JSON.parse(sessionStorage.getItem("searchState"));
-
-    if (searchState == null || searchState.paramPath !== window.location.pathname){
+    const queryString = window.location.search;
+    if (searchState == null || searchState.paramPath !== window.location.pathname || queryString === ""){
         initSearchState();
         return null;
     }
 
     searchState.page = getCurrentPage();
+
+    const urlParams = new URLSearchParams(queryString);
+    urlParams.forEach(
+        (value, key) => {
+            searchState[key] = value;
+        }
+    );
     sessionStorage.setItem("searchState", JSON.stringify(searchState));
 
 })
 
-document.addEventListener('DOMContentLoaded', function(){
-    const form = document.getElementById('searchForm');
-    const value = getSearchState();
-
-    form.addEventListener('input', () => {
-        form.querySelectorAll('input').forEach((input) => {
-            value[input.name] = input.value;
-        });
-        sessionStorage.setItem("searchState", JSON.stringify(value));
-    });
-});
+// document.addEventListener('DOMContentLoaded', function(){
+//     const form = document.getElementById('searchForm');
+//     const value = getSearchState();
+//
+//     form.addEventListener('input', () => {
+//         form.querySelectorAll('input').forEach((input) => {
+//             value[input.name] = input.value;
+//         });
+//         sessionStorage.setItem("searchState", JSON.stringify(value));
+//     });
+// });

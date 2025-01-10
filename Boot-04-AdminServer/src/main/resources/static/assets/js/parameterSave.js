05dbf4cc53c2938a15e731d698cc1415786eb61a
neobnsrnd4 <neobns.rnd.4@gmail.com>
2025-01-10 17:42:35 +0900

    Update parameterSave.js

--- Boot-04-AdminServer/src/main/resources/static/assets/js/parameterSave.js
+++ Boot-04-AdminServer/src/main/resources/static/assets/js/parameterSave.js
@@ -1,3 +1,8 @@
+/*
+이 자바스크립트 파일은 검색 페이지(중요)의 상단에 삽입하여야 한다.
+ */
+
+
 function getSearchState(){
     return JSON.parse(sessionStorage.getItem("searchState"));
 }
@@ -6,8 +11,10 @@ function getCurrentPage(){
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     let currentPage = urlParams.get('page');
+
+    //기본 페이지는 1
     if (currentPage == null){
-        currentPage = '1'
+        currentPage = '1';
     }
     return currentPage;
 }
@@ -19,31 +26,45 @@ function initSearchState(){
     sessionStorage.setItem("searchState", JSON.stringify(value));
 }
 
-const collectParams = () => {
-
-};
-
+//DOM이 준비되었을 때 자동으로 실행
 document.addEventListener('DOMContentLoaded', function(){
-    const searchState = JSON.parse(sessionStorage.getItem("searchState"));
+    const searchState = getSearchState();
+    const queryString = window.location.search;
 
-    if (searchState == null || searchState.paramPath !== window.location.pathname){
+    //searchState 가 스토리지에 저장되어 있지 않을 때
+    //searchState 파라미터가 이 페이지에서 쓰이는 파라미터가 아닐 때
+    //이 페이지에서 쓰이는 파라미터는 맞지만 현재 겟 쿼리 없이 해당 페이지에 재접속했을 때
+    //searchState 초기화를 수행하고 함수를 종료한다.
+    if (searchState == null || searchState.paramPath !== window.location.pathname || queryString === ""){
         initSearchState();
         return null;
     }
 
-    searchState.page = getCurrentPage();
+    //겟 쿼리를 분석하여 파라미터 정보 최신화
+    const urlParams = new URLSearchParams(queryString);
+    urlParams.forEach(
+        (value, key) => {
+            if (key === 'page'){
+                searchState.page = getCurrentPage();
+            } else {
+                searchState[key] = value;
+            }
+        }
+    );
+
+    //최종 반영
     sessionStorage.setItem("searchState", JSON.stringify(searchState));
 
 })
 
-document.addEventListener('DOMContentLoaded', function(){
-    const form = document.getElementById('searchForm');
-    const value = getSearchState();
-
-    form.addEventListener('input', () => {
-        form.querySelectorAll('input').forEach((input) => {
-            value[input.name] = input.value;
-        });
-        sessionStorage.setItem("searchState", JSON.stringify(value));
-    });
-});
+// document.addEventListener('DOMContentLoaded', function(){
+//     const form = document.getElementById('searchForm');
+//     const value = getSearchState();
+//
+//     form.addEventListener('input', () => {
+//         form.querySelectorAll('input').forEach((input) => {
+//             value[input.name] = input.value;
+//         });
+//         sessionStorage.setItem("searchState", JSON.stringify(value));
+//     });
+// });

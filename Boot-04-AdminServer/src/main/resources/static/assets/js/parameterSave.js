0f1f047a019a463f21aa73ab11d015c09d8b05fd
neobnsrnd4 <neobns.rnd.4@gmail.com>
2025-01-10 13:47:02 +0900

    디자인 통일, parameterSave.js 추가

---
+++ Boot-04-AdminServer/src/main/resources/static/assets/js/parameterSave.js
@@ -0,0 +1,49 @@
+function getSearchState(){
+    return JSON.parse(sessionStorage.getItem("searchState"));
+}
+
+function getCurrentPage(){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let currentPage = urlParams.get('page');
+    if (currentPage == null){
+        currentPage = '1'
+    }
+    return currentPage;
+}
+
+function initSearchState(){
+    const pathName = window.location.pathname;
+    const currentPage = getCurrentPage();
+    const value = {"paramPath": pathName, "page":currentPage};
+    sessionStorage.setItem("searchState", JSON.stringify(value));
+}
+
+const collectParams = () => {
+
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+    const searchState = JSON.parse(sessionStorage.getItem("searchState"));
+
+    if (searchState == null || searchState.paramPath !== window.location.pathname){
+        initSearchState();
+        return null;
+    }
+
+    searchState.page = getCurrentPage();
+    sessionStorage.setItem("searchState", JSON.stringify(searchState));
+
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.getElementById('searchForm');
+    const value = getSearchState();
+
+    form.addEventListener('input', () => {
+        form.querySelectorAll('input').forEach((input) => {
+            value[input.name] = input.value;
+        });
+        sessionStorage.setItem("searchState", JSON.stringify(value));
+    });
+});

cfeb74e861a2c496eca951fd22852c61d5d99c21
neobnsrnd4 <neobns.rnd.4@gmail.com>
2025-02-06 14:03:19 +0900

    패키지 통합으로 componentscan 제거

--- Boot-04-AdminServer/src/main/java/neo/spider/admin/AdminServerApplication.java
+++ Boot-04-AdminServer/src/main/java/neo/spider/admin/AdminServerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAdminServer
-@ComponentScan(basePackages = {"neo.spider.admin.common", "neo.spider.admin.e2e", "neo.spider.admin.batch"}) // 패키지가 분리되었으므로 컴포넌트를 스캔할 패키지를 직접 명시
 public class AdminServerApplication {
 
 	public static void main(String[] args) {

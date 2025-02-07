82445860cefdabec86eacf680aaf51c5ad18414f
neobnsrnd1 <neobns.rnd1@gmail.com>
2025-02-07 18:16:21 +0900

    package admin -> solution

---
+++ wiremock-service-wm/src/main/java/neo/spider/solution/controller/AdminController.java
@@ -0,0 +1,40 @@
+package neo.spider.solution.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.RequiredArgsConstructor;
+import neo.spider.solution.dto.AdminApiDTO;
+import neo.spider.solution.service.AdminService;
+
+@Controller
+@RequiredArgsConstructor
+public class AdminController {
+
+	private final AdminService adminService;
+	
+	@GetMapping("/admin/mock")
+	public String showAdmin(
+			@RequestParam(defaultValue = "1") int page,   
+            @RequestParam(defaultValue = "10") int size,  
+            @RequestParam(required = false) String mockApiName,  
+            Model model) {
+
+		 // 검색 및 페이징 처리된 API 목록 가져오기
+        List<AdminApiDTO> apiList = adminService.findApisByPage(page, size, mockApiName);
+        int totalApis = adminService.countApis(mockApiName);
+        int totalPages = (int) Math.ceil((double) totalApis / size);
+
+        // Model에 데이터 추가
+        model.addAttribute("apiList", apiList);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("searchKeyword", mockApiName); // 검색어 유지
+		return "mock/api_list";
+	}
+	
+}

d06f5f31440d84400aa18a6e3e06804232ff0856
neobnsrnd4 <neobns.rnd.4@gmail.com>
2025-02-13 13:10:42 +0900

    배치 모니터링 상태 badge 동적 적용

---
+++ Boot-04-AdminServer/src/main/resources/static/assets/js/batch/status_badge.js
@@ -0,0 +1,14 @@
+$('.exec-status').each(function() {
+    const execStatus = $(this).text();
+    let statusClass = '';
+
+    if(execStatus === 'COMPLETED'){
+        statusClass = 'bg-success';
+    } else if(execStatus === 'FAILED' || execStatus === 'UNKNOWN'){
+        statusClass = 'bg-danger';
+    } else {
+        statusClass = 'bg-warning';
+    }
+
+    $(this).parent().addClass(statusClass);
+})
